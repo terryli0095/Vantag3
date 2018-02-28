2091e59cbe194edafca1337f6d13e73b14c0ff99
terryli0095 <terryli0095@gmail.com>
2018-02-28 11:58:43 -0500

    change to erc20

--- contracts/CampaignFund.sol
+++ contracts/CampaignFund.sol
@@ -1,108 +1,101 @@
 pragma solidity ^0.4.19;
 
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import './interfaces/ERC223_receiving_contract.sol';
 
-contract CampaignFund {
-    
-    struct Contribution {
-     address id;
-     uint total;
-    }
-    
-    uint256 private constant unit_wei = 1000000000000000000;
-    address private creator;
-    address public candidateId;
-    address[] private payees;
-    Contribution [] private contributors; // contributor uport id
-    
-    event LogCoinsSent(address deliveredTo, uint amount);
-    event LogContribution(address uportId, uint amount);
-    event LogNewPayeeAdded (string ref, address payee);
-    event LogExceedAllowableAmount (address contributor);
-    event LogInsuficientFund (string error);
+contract CampaignFund is Ownable, ERC223ReceivingContract {
 
-    function CampaignFund() public payable {
-        creator = msg.sender;
-    }
-    
-    function candidateuPortId(address id) public {
-        candidateId = id;
-    }
-    
-    function contribute(address contributorId) public payable {
-        if (msg.value > 2*unit_wei ) { // reject when amount is greater than 2 ETH
-            revert(); 
-        }
-        uint32 i = 0;
-        for (i = 0; i < contributors.length; i++) {
-            if (contributors[i].id == contributorId) { //found the contributor
-                if ( (contributors[i].total + msg.value) > 2*unit_wei ) {
-                    LogExceedAllowableAmount(contributorId);
-                    revert();
-                } else {
-                    contributors[i].total += msg.value;
-                    LogContribution(contributorId, contributors[i].total);
-                    break;
-                }
-            }
-        }
-        if (i == contributors.length || i == 0) { 
-            // new contributor 
-            Contribution memory newContributor;
-            newContributor.total = msg.value; 
-            newContributor.id = contributorId;
-            contributors.push(newContributor);
-            LogContribution(contributorId, msg.value);
-        } 
-    }
- 
-    function getFundBalance() public constant returns(uint256 bal) { 
-        return (this.balance);                         
-    } 
-    
-    function checkPayeeExists(address payee) internal returns(address) {
-        for ( uint32 i = 0; i < payees.length; i++) {
-            if (payee == payees[i]) {
-                return payee;
-            }
-            else {
-                return 0;
-            }
-        }
+    using SafeMath for uint256;
+
+    mapping(address => uint8) private _owners;
+
+
+    struct Transaction {
+      address from;
+      address to;
+      uint amount;
+      uint8 signatureCount;
+      mapping (address => uint8) signatures;
     }
 
-    function pay(address to, uint256 amount) public {
-        if (checkPayeeExists(to) != address(0)) {
-             payees.push(to);
-             LogNewPayeeAdded ('new payee', to);
-        }
-        uint256 payment = amount;
-        if (this.balance >= payment) {
-            to.transfer(payment);
-            LogCoinsSent (to, payment);
-        }
-        else {
-            LogInsuficientFund ("not enough fund!");
-            revert();
-        }
-        
+    mapping (uint => Transaction) private _transactions;
+    uint[] private _pendingTransactions;
+
+
+    uint private transactionIdx;
+    uint constant MIN_SIGNATURES = 10;
+    address tokenAddress;
+    uint maxDonation;
+    CampaignRegistry Registry;
+
+    mapping (address => uint) ContributionTracker;
+
+    event ownerAdded(address newOwner);
+    event ownerRemoved(address deletedOwner);
+
+    event Contribution(address contributor, address reciever, uint amount, uint timestamp);
+
+    event transactionCreated(address campagin, address reciever, uint amount, uint timestamp);
+    event TransactionCompleted(address from, address to, uint amount, uint transactionId, uint timestamp);
+    event TransactionSigned(address by, uint transactionId);
+
+    modifier validOwner() {
+        require(msg.sender == _owner || _owners[msg.sender] == 1);
+        _;
     }
-    
-    function contributorList() public returns (Contribution[]){
-        return (contributors);
-        
+
+
+    function CampaignFund( uint _maxDonation ) public {
+        Registry= CampaignRegistry(0xabc);
+        tokenAddress = 0x123;
+        Registry.addCampaignID(this,msg.sender);
+        maxDonation= _maxDonation;
     }
-    
-    function payeeList() public returns (address[] ) {
-        return (payees);
+
+    function addOwner(address _owner)
+      onlyOwner
+      public {
+        _owners[owner] = 1;
+        ownerAdded(_owner);
+      }
+
+    function removeOwner(address owner)
+      onlyOwner
+      public {
+        _owners[owner] = 0;
+        ownerRemoved(_owner);
+      }
+
+
+    function contribute(uint _amount) public {
+        require (ContributionTracker[msg.sender]+_amount <= maxDonation );
+        require(Registry.isContributor(msg.sender));
+        require(ERC20(tokenAddress).transferFrom(msg.sender, this, _amount));
+        ContributionTracker[msg.sender]+= _amount;
+
     }
-    
+
+    function tokenFallback(address _from, uint _value, bytes _data) public {
+        require(Registry.isContributor(_from));
+        require(ContributionTracker[_from] + _value <= maxDonation);
+        Contribution(_from, this, _value, now);
+    }
+
+
+    function getFundBalance() public constant returns(uint256) {
+        return (ERC20(tokenAddress).balanceOf(this));
+    }
+
+
+
     /**********
-     Standard kill() function to recover funds 
+     Standard kill() function to recover funds
      **********/
     function kill() external {
         if (msg.sender == creator) {
             selfdestruct(creator);
         }
     }
-        
+
 }
